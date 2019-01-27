c3bccfc28c8a8bc15af38165bf213ef53fce6087
ZhouYu <muyu.zhouyu@outlook.com>
2019-01-27 13:16:13 +0800

    Finish 引入css样式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import { Table, TableColumn, Button } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
